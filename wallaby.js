89ccf1d298e55d63453f4c5efb4a6b5eb32ebf32
Ziv Levy <zivl@wix.com>
2019-01-07 11:07:41 +0200

    add wallabyjs

---
+++ wallaby.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+  return {
+    files: ['index.js'],
+
+    tests: ['test/**/*.test.js'],
+
+    env: {
+      type: 'node'
+    },
+    testFramework: 'jest'
+  };
+};

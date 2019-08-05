ce4859224166eaf5640c95b8089ce44f013414a8
Tomer Ohana <tomero@wix.com>
2019-08-05 09:29:06 +0300

    Remove semicolons

--- test/node.test.js
+++ test/node.test.js
@@ -1,10 +1,10 @@
-const NodeSentry = require('@sentry/node');
-const sentryTestkit = require('../index.js');
-const { createCommonTests } = require('./commonTests');
+const NodeSentry = require('@sentry/node')
+const sentryTestkit = require('../index.js')
+const { createCommonTests } = require('./commonTests')
 
-const { testkit, sentryTransport } = sentryTestkit();
-const DUMMY_DSN = 'https://acacaeaccacacacabcaacdacdacadaca@sentry.io/000001';
-const Sentry = NodeSentry;
+const { testkit, sentryTransport } = sentryTestkit()
+const DUMMY_DSN = 'https://acacaeaccacacacabcaacdacdacadaca@sentry.io/000001'
+const Sentry = NodeSentry
 describe('sentry test-kit test suite - @sentry/node', function() {
   beforeAll(() =>
     Sentry.init({
@@ -12,13 +12,13 @@ describe('sentry test-kit test suite - @sentry/node', function() {
       release: 'test',
       transport: sentryTransport,
       beforeSend(event) {
-        event.extra = { os: 'mac-os' };
-        return event;
+        event.extra = { os: 'mac-os' }
+        return event
       },
     })
-  );
+  )
 
-  beforeEach(() => testkit.reset());
+  beforeEach(() => testkit.reset())
 
-  createCommonTests({ Sentry, testkit });
-});
+  createCommonTests({ Sentry, testkit })
+})

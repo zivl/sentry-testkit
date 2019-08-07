d75e0b81cd96405068dd488d3c88d0834f9afa60
Tomer Ohana <tomero@wix.com>
2019-08-07 13:35:09 +0300

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { Event, Exception, Transport, TransportOptions } from '@sentry/types';
+import { Event, Breadcrumb, User, Severity, StackFrame, Transport, TransportOptions } from '@sentry/types';
 
 export = sentryTestkit;
 
@@ -10,15 +10,33 @@ declare namespace sentryTestkit {
     removeListener(event: string, handler: (...args: any[]) => any): void;
   }
 
+  interface ReportError {
+    name: string
+    message: string
+    stacktrace: StackFrame[]
+  }
+
+  interface Report {
+    breadcrumbs: Breadcrumb[]
+    error?: ReportError
+    message?: string
+    extra?: { [key: string]: any }
+    level: Severity
+    release?: string
+    user?: User
+    tags: { [key: string]: string }
+    originalReport: Event
+  }
+
   export interface Testkit {
     puppeteer: {
       startListening(page: Page): void;
       stopListening(page: Page): void;
     };
-    reports(): Event[];
+    reports(): Report[];
     reset(): void;
-    getExceptionAt(index: number): Exception;
-    findReport(e: Error): Event;
+    getExceptionAt(index: number): ReportError;
+    findReport(e: Error): Report;
     isExist(e: Error): boolean;
   }
 

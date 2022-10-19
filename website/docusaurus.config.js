3be9f31ac8bd3324387a225759bcd026fb9b37c8
Ziv Levy <levi.ziv@gmail.com>
2022-10-19 12:26:01 +0300

    docs: fix base url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Sentry-Testkit',
   tagline: 'Testing your reports made easy',
   url: 'https://wix.github.io/',
-  baseUrl: '/',
+  baseUrl: '/sentry-testkit/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',

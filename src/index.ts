ba4310a0feb5579964bbd695a7d4874087352df5
Tomer Ohana <tomero@wix.com>
2022-11-10 17:14:20 +0000

    preserve compatibility with code accessing the `default` field

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { createLocalServerApi } from './localServerApi'
 import { createInitNetworkInterceptor } from './initNetworkInterceptor'
 import { createSentryTransport } from './sentryTransport'
 
-export = () => {
+const create = () => {
   const testkit = createTestkit()
   const sentryTransport = createSentryTransport(testkit)
   const initNetworkInterceptor = createInitNetworkInterceptor(testkit)
@@ -16,3 +16,6 @@ export = () => {
     localServer,
   }
 }
+
+create.default = create
+export = create

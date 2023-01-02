08beae41672f8f059fd3e81f0c65bba724a0ff42
Liam Jones <liam@stardive.co.uk>
2023-01-02 07:57:54 +0000

    Fix jest-mock and reinstate tests (#155)
    The mock was trying to import the same module it was mocking, resulting in a circular reference, now using `jest.requireActual()` to grab the real Sentry for use by the mock.

--- src/jestMock.ts
+++ src/jestMock.ts
@@ -1,14 +1,15 @@
-import * as Sentry from '@sentry/browser'
 import { BrowserOptions } from '@sentry/browser'
 import sentryTestkit from './index'
 
 const { testkit, sentryTransport } = sentryTestkit()
 
+const Sentry = jest.requireActual('@sentry/browser')
+
 jest.mock('@sentry/browser', () =>
   Object.assign({}, Sentry, {
     init: (options: BrowserOptions) =>
       Sentry.init(Object.assign({}, options, { transport: sentryTransport })),
-  })
+  }),
 )
 
 export default testkit
